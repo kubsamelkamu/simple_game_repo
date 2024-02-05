49c638c0c1f12954f609cfd60c207ea6f64deee2
kubsamelkamu <kubsamlkm@gmail.com>
2024-02-05 23:52:15 +0300

    edited script.js file for calculating the overall point of the both player

--- script.js
+++ script.js
@@ -18,14 +18,15 @@ function playGame(player_selection,computer_selection){
        ( player_selection == 'SCISSOR' && computer_selection == 'PAPER') ||
        (player_selection == 'PAPER' && computer_selection === 'ROCK')) {
 
-        computer_score += 1;
-   }else{
         player_score += 1;
+        confirm('human player win this game-round');
+   }else{
+        computer_score += 1;
+        confirm('computer player win this game-round');
    }
     
 
 }
-
 
 while (number_of_game > 0) {
     let playerselection = prompt("select Rock|Paper|Scissor: ").toUpperCase();
@@ -34,22 +35,16 @@ while (number_of_game > 0) {
     number_of_game -= 1;
 }
 
-/*
-function ScorePoint(scorefor = 'computer_win'){
-    if (scorefor === 'computer_win') {
-       computer_score++;
-    }else if(scorefor === 'user_win'){
-        player_score++;
-    }
 
-}*/
+
 winnerOfGame(computer_score,player_score);
 
 function winnerOfGame(computer_score,user_score){
+    const number_of_game = computer_score + player_score;
     if(computer_score > user_score){
-        console.log('Computer win the game by getting the score ' + computer_score + 'out of ' + number_of_game);
+        console.log('Computer win the game by getting the score ' + computer_score + ' out of ' + number_of_game);
     }else if(computer_score < user_score){
-        console.log('Human player win the game by getting the score ' + user_score + 'out of ' + number_of_game);
+        console.log('Human player win the game by getting the score ' + user_score + ' out of ' + number_of_game);
     }else{
         console.log('There is no winner');
     }
